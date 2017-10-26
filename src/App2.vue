809d4f2cf982cb52e086365d1737b60599e1e739
Hunter1110 <wenfeng_zhang@aopen.com>
2017-10-26 10:05:55 +0800

    add app-2 for bind DOM attribute

---
+++ src/App2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span v-bind:title="message">
+      鼠标悬停几秒钟查看此处动态绑定的提示信息!
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: '页面加载于' + new Date().toLocaleString()
+    };
+  }
+}
+</script>
+
+<style>
+#app-2 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import App2 from './App2.vue';
+
 
 window.onload = () => {
   const elApp = document.createElement('div');
   elApp.id = 'app';
   document.body.appendChild(elApp);
 
+  const elApp2 = document.createElement('div');
+  elApp2.id = 'app-2';
+  document.body.appendChild(elApp2);
+
   new Vue({
     el: '#app',
     render: h => h(App)
   });
+
+  new Vue({
+    el: '#app-2',
+    render: h => h(App2)
+  })
 };
 
